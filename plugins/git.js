c67a14ab0a2fb9f70a913db409b2118ec78f2cf9
oggysir <84663122+zbrt-y@users.noreply.github.com>
2021-11-06 23:15:29 -0400

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,22 +7,24 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-M6858-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://imgur.com/a/y7HFCdO", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*M6858 bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*M6858 bot created by ZBRT*
+*Creator number : wa.me/9114842842183?text=Hi%ZBRT%20
 
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
+*CREATOR INSTAGRAM : https://instagram.com/klsiy *
 
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/M6858*
+*Githublink (Setup)  :    https://github.com/zbrt-y/M6858*
 
-*Audio commads :   https://github.com/ameer-kallumthodi/M6858/tree/master/uploads*
+*Audio commads :   https://github.com/zbrt-y/M6858/tree/master/uploads*
 
-*Sticker commads : https://github.com/ameer-kallumthodi/M6858/tree/master/uploads*
+*Sticker commads : https://github.com/zbrt-y/M6858/tree/master/uploads*
 
-*Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
+*Website    : https://zbrt.github.io
+  
 
-*For More Updates Subscribe The Channel ☝*
+
+*For More Updates FOLLOW INSTAGRAM*
 `}) 
 
 }));
